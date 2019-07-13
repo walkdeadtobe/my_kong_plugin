11b0ac0ca39bd9538a32418cde84e26cb06d6b99
walkdeadtobe <2017hemu@gmail.com>
2019-07-13 22:53:59 +0800

    #

---
+++ myplugin/schema.lua
@@ -0,0 +1,12 @@
+local typedefs = require "kong.db.schema.typedefs"
+return {
+  no_consumer = true, -- this plugin is available on APIs as well as on Consumers,
+  fields = {
+    -- Describe your plugin's configuration's schema here.
+    
+  },
+  self_check = function(schema, plugin_t, dao, is_updating)
+    -- perform any custom verification
+    return true
+  end
+}
