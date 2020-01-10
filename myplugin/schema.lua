ab93d5bfa4dd76e35203dc348834c2c60a919e81
walkdeadtobe <2017hemu@gmail.com>
2020-01-10 11:28:19 +0800

    #

--- myplugin/schema.lua
+++ myplugin/schema.lua
@@ -1,32 +1,21 @@
 local typedefs = require "kong.db.schema.typedefs"
 return {
-  name = "kong-plugin-myplugin"
+  no_consumer = true,
   --no_consumer = true, -- this plugin is available on APIs as well as on Consumers,
   fields = {
     -- Describe your plugin's configuration's schema here.
-    { consumer = typedefs.no_consumer },
-    { run_on = typedefs.run_on_first },
-    { protocols = typedefs.protocols_http },
-    { config = {
-      type = "record",
-      fields = {
-        { sso_domain = { type = "string", default = "http://sso-smart.cast.org.cn:8080"},}
-        { check_path = { type = "string", default = "/oauth/check_token" },}
-        { front_domain = { type = "string", default = "http://smart.cast.org.cn"},}
-        { client = {
+    sso_domain = { type = "string", default = "http://sso-smart.cast.org.cn:8080"},
+    check_path = { type = "string", default = "/oauth/check_token" },
+    front_domain = { type = "string", default = "http://smart.cast.org.cn"},
+    client = {
             type = "record",
             --required = true,
             fields = {
               { kexie = { type = "string", default = "sso_domain/oauth/authorize?client_id=kexie&redirect_uri=/oauth/code?back_to=front_domain/ep/cookie.html&response_type=code&scope=read"},}
               { talent = { type = "string", default = "sso_domain/oauth/authorize?client_id=talent&redirect_uri=/oauth/code?back_to=front_domain/ep/cookie_talent.html&response_type=code&scope=read"},}
             },
-          },}, 
-        },
-        
-        --{ anonymous = { type = "string", uuid = true, legacy = true }, },
-        --{ key_in_body = { type = "boolean", default = false }, },
-      },
-  }, },
+          }, 
+  },
   self_check = function(schema, plugin_t, dao, is_updating)
     -- perform any custom verification
     return true
