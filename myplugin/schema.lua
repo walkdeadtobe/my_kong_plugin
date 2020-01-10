07359511da48ec839f5f01d186d00d25042eaa2b
walkdeadtobe <2017hemu@gmail.com>
2020-01-10 11:11:22 +0800

    #

--- myplugin/schema.lua
+++ myplugin/schema.lua
@@ -1,6 +1,6 @@
 local typedefs = require "kong.db.schema.typedefs"
 return {
-  name = "myplugin"
+  name = "kong-plugin-myplugin"
   --no_consumer = true, -- this plugin is available on APIs as well as on Consumers,
   fields = {
     -- Describe your plugin's configuration's schema here.
@@ -10,38 +10,23 @@ return {
     { config = {
       type = "record",
       fields = {
-        { 
-          sso_domain = {
-            type = "string",
-            default = "http://sso-smart.cast.org.cn:8080"
-          },
-          check_path = {
-            type = "string",
-            default = "/oauth/check_token"
-          },
-          front_domain = {
-            type = "string",
-            default = "http://smart.cast.org.cn"
-          },
-          client = {
-            type = "table",
+        { sso_domain = { type = "string", default = "http://sso-smart.cast.org.cn:8080"},}
+        { check_path = { type = "string", default = "/oauth/check_token" },}
+        { front_domain = { type = "string", default = "http://smart.cast.org.cn"},}
+        { client = {
+            type = "record",
             --required = true,
-            default = {
-            kexie = "sso_domain/oauth/authorize?client_id=kexie&redirect_uri=/oauth/code?back_to=front_domain/ep/cookie.html&response_type=code&scope=read",
-            talent = "sso_domain/oauth/authorize?client_id=talent&redirect_uri=/oauth/code?back_to=front_domain/ep/cookie_talent.html&response_type=code&scope=read",
+            fields = {
+              { kexie = { type = "string", default = "sso_domain/oauth/authorize?client_id=kexie&redirect_uri=/oauth/code?back_to=front_domain/ep/cookie.html&response_type=code&scope=read"},}
+              { talent = { type = "string", default = "sso_domain/oauth/authorize?client_id=talent&redirect_uri=/oauth/code?back_to=front_domain/ep/cookie_talent.html&response_type=code&scope=read"},}
             },
-          },
-
-
-          }, 
+          },}, 
         },
         
         --{ anonymous = { type = "string", uuid = true, legacy = true }, },
         --{ key_in_body = { type = "boolean", default = false }, },
       },
   }, },
-    
-  },
   self_check = function(schema, plugin_t, dao, is_updating)
     -- perform any custom verification
     return true
